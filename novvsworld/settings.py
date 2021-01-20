b0208eb6c778a71b5437410459aa96e4d67d8ad3
elijahomolo <eomolo11@gmail.com>
2021-01-20 12:07:41 -0600

    fixed mobile ui

--- novvsworld/settings.py
+++ novvsworld/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'nw_site.apps.NwSiteConfig',
+    'django_render_partial',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
